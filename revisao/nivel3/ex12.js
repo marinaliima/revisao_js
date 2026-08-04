8495f5ca75bf1c3ba3bbaf436a34f526acb963ad
marinaliima <marina.l@edu.senai.br>
2026-08-03 22:52:39 -0300

    adiciona exercícios 12 e 13

--- revisao/nivel3/ex12.js
+++ revisao/nivel3/ex12.js
@@ -1,1 +1,34 @@
 //12. Caixa da Loja
+
+let produtos = [Number(prompt("Digite o valor do produto (0 para parar): "))];
+let valorTotal = produtos[0];
+let maisCaro = produtos[0];
+let maisBarato = produtos[0];
+
+for (i = 1; produtos[produtos.length - 1] !== 0; i++){
+    produtos.push(Number(prompt("Digite o valor do produto (0 para parar): ")));
+    valorTotal += produtos[i];
+            
+    if (produtos[i] > maisCaro){
+        maisCaro = produtos[i];
+    }
+
+    if ((produtos[i] !== 0) && (produtos[i] < maisBarato)){
+        maisBarato = produtos[i];
+    }
+}
+
+produtos.pop();
+
+let media = valorTotal / produtos.length;
+
+alert("Você comprou " + produtos.length + " produtos.");
+console.log("Você comprou " + produtos.length + " produtos.");
+alert("Valor total: R$" + valorTotal);
+console.log("Valor total: R$" + valorTotal);
+alert("Produto mais caro: R$" + maisCaro);
+console.log("Produto mais caro: R$" + maisCaro);
+alert("Produto mais barato: R$" + maisBarato);
+console.log("Produto mais barato: R$" + maisBarato);
+alert("A média dos preços foi R$" + media);
+console.log("A média dos preços foi R$" + media);
--- revisao/nivel3/ex13.js
+++ revisao/nivel3/ex13.js
@@ -1,1 +1,34 @@
 //13. Pesquisa de Idade
+
+let idadePessoas = [18, 16, 10, 26, 19, 17, 15, 23, 22, 12, 15, 14, 17, 17, 19];
+let maior = idadePessoas[0];
+let menor = idadePessoas[0];
+let soma = 0;
+let maiorIdade = 0;
+let menorIdade = 0;
+
+for (i = 0; i < idadePessoas.length; i++){
+    soma += idadePessoas[i];
+
+    if (idadePessoas[i] > maior){
+        maior = idadePessoas[i];
+    }
+
+    if (idadePessoas[i] < menor){
+        menor = idadePessoas[i];
+    }
+
+    if (idadePessoas[i] >= 18){
+        maiorIdade++;
+    } else {
+        menorIdade++;
+    }
+}
+
+let media = soma / idadePessoas.length;
+
+console.log("A maior idade cadastrada é: " + maior + ".");
+console.log("A menor idade cadastrada é: " + menor + ".");
+console.log("A média das idades cadastradas é: " + media + ".");
+console.log(maiorIdade + " pessoas são maiores de idade.");
+console.log(menorIdade + " pessoas são menores de idade.");
