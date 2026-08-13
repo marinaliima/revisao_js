7271d1f592fb96e5105300c5ad3cc42d3d5082cc
marinaliima <marina.l@edu.senai.br>
2026-08-13 16:18:30 -0300

    adiciona exercício 8

--- revisao/nivel2/ex8.js
+++ revisao/nivel2/ex8.js
@@ -1,1 +1,22 @@
 //8. Campeonato
+
+let vitorias = Number(prompt('Digite o número de vitórias:'));
+let empates = Number(prompt('Digite o número de empates:'));
+let derrotas = Number(prompt('Digite o número de derrotas:'));
+
+let partidasJogadas = vitorias + empates + derrotas;
+let pontuacaoObtida = vitorias * 2 + empates * 1;
+let pontuacaoMaxima = partidasJogadas * 2;
+let aproveitamento = (pontuacaoObtida * 100) / pontuacaoMaxima;
+
+alert(`Pontuação: ${pontuacaoObtida} \nAproveitamento: ${aproveitamento}%`);
+
+if (aproveitamento >= 90) {
+    alert("O time foi excelente!");
+} else if (aproveitamento >= 70) {
+    alert("O time foi bem!");
+} else if (aproveitamento >= 50) {
+    alert("O time foi regular.");
+} else {
+    alert("O time foi péssimo.");
+}
