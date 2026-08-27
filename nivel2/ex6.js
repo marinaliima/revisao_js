80e6e6dbcc51060dd8ba5128ae6b3edbe377a994
marinaliima <marina.l@edu.senai.br>
2026-08-27 08:58:49 -0300

    fix: arruma exercício 6

--- nivel2/ex6.js
+++ nivel2/ex6.js
@@ -10,8 +10,10 @@ console.log(lados);
 if ((l1 === 0) || (l2 === 0) || l3 === 0) {
     console.log("Esses valores não podem formar um triângulo.");
     alert("Esses valores não podem formar um triângulo.");
+
 } else {
-    if ((l1 !== l2) && (l2 !== l3)){
+
+    if ((l1 !== l2) && (l1 !== l3) && (l2 !==l3)){
         console.log("Esses valores formam um triângulo escaleno.");
         alert("Esses valores formam um triângulo escaleno.");
     } else if ((l1 === l2) && (l2 === l3)) {
